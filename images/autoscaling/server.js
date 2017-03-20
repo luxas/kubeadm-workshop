3749786a5463816c518f2c404007f8e5db5ddac3
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2017-03-20 08:54:02 +0200

    Base the autoscaling image on the nodejs image and fixup the metrics endpoint

--- images/autoscaling/server.js
+++ images/autoscaling/server.js
@@ -23,8 +23,8 @@ http.createServer(function(request, response) {
   response.writeHead(200);
 
   if (request.url == "/metrics") {
-    response.write("# HELP http_requests_total The amount of requests served by the server in total\n# TYPE http_requests_total counter\nhttp_requests_total " + totalrequests + "\n");
-    response.end("# HELP http_requests_per_second The amount of requests per second served by the server the latest five seconds\n# TYPE http_requests_per_second gauge\nhttp_requests_per_second " + avgQps + "\n");
+    response.write("# HELP http_requests_total The amount of requests served by the server in total\nTYPE http_requests_total counter\nhttp_requests_total " + totalrequests + "\n");
+    response.end("# HELP http_requests_per_second The amount of requests per second served by the server the latest five seconds\nTYPE http_requests_per_second gauge\nhttp_requests_per_second " + avgQps + "\n");
     return;
   }
   response.end("Hello! My name is " + os.hostname() + ". The last " + resolution + " seconds, the average QPS has been " + avgQps + ". Total requests served: "+ totalrequests + "\n");
