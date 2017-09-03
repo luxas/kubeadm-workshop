273e64b6fb21c540a1a622daebd51f1a70a5196b
Lucas Käldström <lucas.kaldstrom@hotmail.co.uk>
2017-09-03 14:46:18 +0300

    Split up e2e-script into init and install

--- init.sh
+++ init.sh
@@ -1,31 +1,30 @@
 
-apt-get update && apt-get install -y apt-transport-https
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
-cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
-deb http://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-apt-get update && apt-get install -y docker.io kubeadm ceph-common
-git clone https://github.com/luxas/kubeadm-workshop
-cd kubeadm-workshop
-
-
 kubeadm init --config kubeadm.yaml
 
 mkdir -p $HOME/.kube
 cp /etc/kubernetes/admin.conf $HOME/.kube/config
 chown $(id -u):$(id -g) $HOME/.kube/config
 
-ROOK_BRANCH=${ROOK_BRANCH:-"release-0.5"}
-
-kubectl apply -f https://git.io/weave-kube-1.6
+kubectl create secret -n kube-system generic weave-passwd --from-literal=weave-passwd=$(hexdump -n 16 -e '4/4 "%08x" 1 "\n"' /dev/random)
+kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&password-secret=weave-passwd"
 kubectl taint nodes --all node-role.kubernetes.io/master-
-curl -sSL https://git.io/kube-dashboard | sed "s|image:.*|image: luxas/dashboard:v1.6.3|" | kubectl apply -f -
-kubectl apply -f demos/monitoring/heapster.yaml
+
+
+curl -sSL https://git.io/kube-dashboard | sed "s|image:.*|image: luxas/kubernetes-dashboard:v1.6.3|" | kubectl apply -f -
+kubectl apply -f demos/dashboard/ingress.yaml
+
+kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml
+curl -sSL https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/heapster.yaml | \
+	sed "s|image:.*|image: luxas/heapster:v1.4.0|" | kubectl apply -f -
+
 # TODO: Start using PVs/PVCs for influxdb and grafana
 kubectl apply -f demos/monitoring/influx-grafana.yaml
+
 kubectl apply -f demos/loadbalancing/traefik-common.yaml
 kubectl apply -f demos/loadbalancing/traefik-ngrok.yaml
-kubectl apply -f demos/dashboard/ingress.yaml
+
+# Install the Rook and Prometheus operators
+ROOK_BRANCH=${ROOK_BRANCH:-"release-0.5"}
 kubectl apply -f https://raw.githubusercontent.com/rook/rook/${ROOK_BRANCH}/cluster/examples/kubernetes/rook-operator.yaml
 kubectl apply -f demos/monitoring/prometheus-operator.yaml
 
@@ -36,6 +35,9 @@ while [[ $(kubectl get prometheus; echo $?) == 1 ]]; do sleep 1; done
 # Requires the Rook and Prometheus API groups
 kubectl apply -f https://raw.githubusercontent.com/rook/rook/${ROOK_BRANCH}/cluster/examples/kubernetes/rook-cluster.yaml
 kubectl apply -f https://raw.githubusercontent.com/rook/rook/${ROOK_BRANCH}/cluster/examples/kubernetes/rook-storageclass.yaml
+kubectl patch storageclass rook-block -p '{"metadata":{"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}'
+
+# Demo the autoscaling based on custom metrics feature
 kubectl apply -f demos/monitoring/sample-prometheus-instance.yaml
 kubectl apply -f demos/monitoring/sample-metrics-app.yaml
 kubectl apply -f demos/monitoring/custom-metrics.yaml
@@ -49,15 +51,14 @@ kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/c
 kubectl create ns wardle
 kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n wardle apply -f -
 
+# Setup helm and install tiller
 helm init
-
 kubectl -n kube-system create serviceaccount tiller
 kubectl -n kube-system patch deploy tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccountName":"tiller"}}}}'
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount kube-system:tiller
+kubectl -n kube-system set image deploy/tiller-deploy tiller=luxas/tiller:v2.6.1
 
-
+# Demo an aggregated API server
 kubectl apply -f demos/sample-apiserver/wardle.yaml
 while [[ $(kubectl get flunder; echo $?) == 1 ]]; do sleep 1; done
 kubectl apply -f demos/sample-apiserver/my-flunder.yaml
-
-#kubectl patch storageclass rook-block -p '{"metadata":{"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}'
---
+++ install.sh
@@ -0,0 +1,50 @@
+
+# Add the kubernetes apt repo
+apt-get update && apt-get install -y apt-transport-https
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+cat <<EOF >/etc/apt/sources.list.d/kubernetes.list
+deb http://apt.kubernetes.io/ kubernetes-xenial main
+EOF
+
+# Install docker and kubeadm
+apt-get update && apt-get install -y docker.io kubeadm ceph-common
+
+# Set current arch
+ARCH=${ARCH:-"amd64"}
+
+# Enable hostPort support using CNI & Weave
+mkdir -p /etc/cni/net.d/
+cat > /etc/cni/net.d/10-mynet.conflist <<EOF
+{
+    "cniVersion": "0.3.0",
+    "name": "mynet",
+    "plugins": [
+        {
+            "name": "weave",
+            "type": "weave-net",
+            "hairpinMode": true
+        },
+        {
+            "type": "portmap",
+            "capabilities": {"portMappings": true},
+            "snat": true
+        }
+    ]
+}
+EOF
+
+# Download portmap which is responsible for the hostPort mappings
+curl -sSL https://github.com/containernetworking/plugins/releases/download/v0.6.0/cni-plugins-${ARCH}-v0.6.0.tgz | tar -xz -C /opt/cni/bin ./portmap
+
+# Install helm
+curl -sSL https://storage.googleapis.com/kubernetes-helm/helm-v2.6.1-linux-${ARCH}.tar.gz | tar -xz -C /usr/local/bin linux-${ARCH}/helm --strip-components=1
+
+# Use overlay as the docker storage driver
+cat /proc/mounts | awk '{print $2}' | grep '/var/lib/docker' | xargs -r umount
+rm -rf /var/lib/docker
+sed -e "s|/usr/bin/dockerd|/usr/bin/dockerd -s overlay|g" -i /lib/systemd/system/docker.service
+systemctl daemon-reload
+systemctl restart docker
+
+git clone https://github.com/luxas/kubeadm-workshop
+cd kubeadm-workshop
