1bafbd36558fc3dbdbdaeb74099eca1b58bba82b
Your Name <you@example.com>
2017-10-07 21:32:32 +0300

    Update the init.sh script; reorder some commands and the metrics server, use new features, etc

--- init.sh
+++ init.sh
@@ -1,24 +1,21 @@
 
 kubeadm init --config kubeadm.yaml
 
-mkdir -p $HOME/.kube
-cp /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
 kubectl create secret -n kube-system generic weave-passwd --from-literal=weave-passwd=$(hexdump -n 16 -e '4/4 "%08x" 1 "\n"' /dev/random)
 kubectl apply -n kube-system -f "https://cloud.weave.works/k8s/net?k8s-version=$(kubectl version | base64 | tr -d '\n')&password-secret=weave-passwd"
 kubectl taint nodes --all node-role.kubernetes.io/master-
 
-
-curl -sSL https://git.io/kube-dashboard | sed "s|image:.*|image: luxas/kubernetes-dashboard:v1.7.1|" | kubectl apply -f -
+# Deploy the Dashboard
+curl -sSL https://raw.githubusercontent.com/kubernetes/dashboard/master/src/deploy/recommended/kubernetes-dashboard.yaml | sed "s|gcr.io/google_containers/kubernetes-dashboard-amd64:.*|luxas/kubernetes-dashboard:v1.7.1|" | kubectl apply -f -
 kubectl apply -f demos/dashboard/ingress.yaml
 
+# Deploy metrics-server and Heapster
+kubectl apply -f demos/monitoring/metrics-server.yaml
 kubectl apply -f https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/rbac/heapster-rbac.yaml
 curl -sSL https://raw.githubusercontent.com/kubernetes/heapster/master/deploy/kube-config/influxdb/heapster.yaml | \
 	sed "s|image:.*|image: luxas/heapster:v1.4.0|" | kubectl apply -f -
-
-# TODO: Start using PVs/PVCs for influxdb and grafana
-kubectl apply -f demos/monitoring/influx-grafana.yaml
 
 kubectl apply -f demos/loadbalancing/traefik-common.yaml
 kubectl apply -f demos/loadbalancing/traefik-ngrok.yaml
@@ -37,26 +34,29 @@ kubectl apply -f https://raw.githubusercontent.com/rook/rook/${ROOK_BRANCH}/clus
 kubectl apply -f https://raw.githubusercontent.com/rook/rook/${ROOK_BRANCH}/cluster/examples/kubernetes/rook-storageclass.yaml
 kubectl patch storageclass rook-block -p '{"metadata":{"annotations": {"storageclass.kubernetes.io/is-default-class": "true"}}}'
 
+echo "Waiting for rook to create a Secret"
+while [[ $(kubectl get secret rook-rook-user; echo $?) == 1 ]]; do sleep 1; done
+
+# Set up the rook Secret for other namespaces than default
+kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n kube-system apply -f -
+kubectl create ns wardle
+kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n wardle apply -f -
+
+
+kubectl apply -f demos/monitoring/influx-grafana.yaml
+
 # Demo the autoscaling based on custom metrics feature
 kubectl apply -f demos/monitoring/sample-prometheus-instance.yaml
 kubectl apply -f demos/monitoring/sample-metrics-app.yaml
 kubectl apply -f demos/monitoring/custom-metrics.yaml
 kubectl create clusterrolebinding allowall-cm --clusterrole custom-metrics-server-resources --user system:anonymous
 
-echo "Waiting for rook to create a Secret"
-while [[ $(kubectl get secret rook-rook-user; echo $?) == 1 ]]; do sleep 1; done
-
-# Requires the rook-rook-user
-kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n kube-system apply -f -
-kubectl create ns wardle
-kubectl get secret rook-rook-user -oyaml | sed "/resourceVer/d;/uid/d;/self/d;/creat/d;/namespace/d" | kubectl -n wardle apply -f -
-
 # Setup helm and install tiller
 helm init
 kubectl -n kube-system create serviceaccount tiller
-kubectl -n kube-system patch deploy tiller-deploy -p '{"spec":{"template":{"spec":{"serviceAccountName":"tiller"}}}}'
+kubectl -n kube-system set serviceaccount deploy tiller-deploy tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount kube-system:tiller
-kubectl -n kube-system set image deploy/tiller-deploy tiller=luxas/tiller:v2.6.2
+kubectl -n kube-system set image deploy/tiller-deploy tiller=luxas/tiller:v2.6.1
 
 # Demo an aggregated API server
 kubectl apply -f demos/sample-apiserver/wardle.yaml
